192553f15f05c94fe2b01550509928d94e2d3d87
CynthiaG-W <cindydetec@gmail.com>
2026-05-08 14:29:12 +0300

    initial commit

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,8 +1,16 @@
+import React from "react";  
 // Code PasswordInput Component Here
 
 function PasswordInput (){
+    function handleChange(event){
+        console.log("Enter password...")
+}
     return(
-        <></>
+        <input
+            type="password"
+            onChange={handleChange}
+            placeholder="Enter password"
+        />
     )
 }
 
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,8 +1,18 @@
 // Code SubmitButton Component Here
 
 function SubmitButton (){
+    function handleEnter(){
+        console.log("Mouse entering!")
+    }
+    function handleLeave(){
+        console.log("Mouse Exiting")
+    }
+
     return(
-        <></>
+        < button onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
+            Submit password
+        </button>
+
     )
 }
 
